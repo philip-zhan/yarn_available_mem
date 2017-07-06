9501c058d9bfd34f3825058b6e09ea04b12db646
Haowen Zhan <h6zhan@uwaterloo.ca>
2017-07-06 19:41:35 +0800

    append original available mem to output

--- get_available_mem.py
+++ get_available_mem.py
@@ -5,4 +5,4 @@ http = urllib2.urlopen(
     "http://172.18.0.5:8088/jmx?get=Hadoop:service=ResourceManager,name=RMNMInfo::LiveNodeManagers").read()
 matches = re.findall(r"UsedMemoryMB\\\":(\d+)", http)
 for match in matches:
-    print str((int(match) + 1023) / 1024) + "GB"
+    print match + " => " + str((int(match) + 1023) / 1024) + "GB"
