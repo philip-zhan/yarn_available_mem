2211a4e6ebef6627d1f163d20562235e93de081c
Haowen Zhan <h6zhan@uwaterloo.ca>
2017-07-06 19:31:33 +0800

    it works

--- get_available_mem.py
+++ get_available_mem.py
@@ -1,4 +1,8 @@
+import re
 import urllib2
 
-content = urllib2.urlopen("http://172.18.0.5:8088/jmx?get=Hadoop:service=ResourceManager,name=RMNMInfo::LiveNodeManagers").read()
-
+http = urllib2.urlopen(
+    "http://172.18.0.5:8088/jmx?get=Hadoop:service=ResourceManager,name=RMNMInfo::LiveNodeManagers").read()
+matches = re.findall(r"UsedMemoryMB\\\":(\d+)", http)
+for match in matches:
+    print str((int(match) + 1023) / 1024) + "GB"
